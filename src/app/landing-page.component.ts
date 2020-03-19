74211cffacbbd5156e688119e7f3e8bf51f8520a
nicholaschaves <nicholaschaves2702@gmail.com>
2020-03-19 16:40:51 -0300

    .

--- src/app/landing-page.component.ts
+++ src/app/landing-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-landing-page',
-    templateUrl: './landing-page.component.html',
-    styleUrls: ['./landing-page.component.scss']
-  })
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
 
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit, OnDestroy {
+
+  navbarOpen: boolean = false;
+
+  showNavBar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
 
 }
